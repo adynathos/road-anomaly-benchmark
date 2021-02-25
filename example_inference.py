6e3ca355ce13b17298140528b5fb6e1bc56113b7
Krzysztof Lis <krzysztof.lis@epfl.ch>
2021-02-25 16:24:59 +0100

    example inference

--- example_inference.py
+++ example_inference.py
@@ -2,24 +2,26 @@
 import numpy as np
 from tqdm import tqdm
 import cv2 as cv
+from road_anomaly_benchmark.evaluation import Evaluation
 
 def method_dummy(image, **_):
-	
+	""" Very naive method: return color saturation """
 	image_hsv = cv.cvtColor(image, cv.COLOR_RGB2HSV_FULL)
-
 	anomaly_p = image_hsv[:, :, 1].astype(np.float) * (1./255.)
-
 	return anomaly_p
 
+
 def main():
-	from road_anomaly_benchmark.evaluation import Evaluation
 
 	ev = Evaluation(method_name = 'Dummy', dataset_name = 'RoadObstacleTrack-test')
 
 	for frame in tqdm(ev.get_dataset()):
+		# run method here
 		result = method_dummy(frame.image)
+		# provide the output for saving
 		ev.save_output(frame, result)
 
+	# wait for the background threads which are saving
 	ev.wait_to_finish_saving()
 
 if __name__ == '__main__':
