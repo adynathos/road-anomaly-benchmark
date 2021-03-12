0d18a87002473d16485147c13d7d85c27b703862
Svenja Uhlemeyer <suhlemeyer@uni-wuppertal.de>
2021-03-12 16:34:08 +0100

    Added DatasetSmallObstacle

--- road_anomaly_benchmark/datasets/__init__.py
+++ road_anomaly_benchmark/datasets/__init__.py
@@ -2,5 +2,5 @@
 from .dataset_registry import DatasetRegistry
 
 # Populate the registry
-from .tracks import DatasetAnomalyTrack, DatasetObstacleTrack
+from .tracks import DatasetAnomalyTrack, DatasetObstacleTrack, DatasetSmallObstacle
 
