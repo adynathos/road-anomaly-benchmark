9683bc1701533b319c2fb063259aa8e6d17ff966
Krzysztof Lis <krzysztof.lis@epfl.ch>
2021-05-11 01:31:58 +0200

    load heatmaps in smaller dimensions

--- road_anomaly_benchmark/paths.py
+++ road_anomaly_benchmark/paths.py
@@ -2,6 +2,6 @@
 from pathlib import Path
 from os import environ
 
-DIR_SRC = Path(__file__).absolute().parents[1]
+DIR_SRC = Path(__file__).resolve().absolute().parents[1]
 DIR_OUTPUTS = Path(environ.get('DIR_OUTPUTS', DIR_SRC / 'outputs'))
 DIR_DATASETS = Path(environ.get('DIR_DATASETS', DIR_SRC / 'datasets'))
